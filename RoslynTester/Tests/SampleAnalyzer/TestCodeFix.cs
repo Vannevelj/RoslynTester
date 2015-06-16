044baa0e7b72679615e6812630ea532f2441d1d6
Vannevelj <jer_vannevel@hotmail.com>
2015-06-16 02:50:13 +0200

    reproduced the MissingMethodException bug

---
+++ RoslynTester/Tests/SampleAnalyzer/TestCodeFix.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+
+namespace Tests.SampleAnalyzer
+{
+    [ExportCodeFixProvider("TestCodeFix", LanguageNames.CSharp), Shared]
+    public class TestCodeFix : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(TestAnalyzer.DiagnosticId);
+        public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+            var methodDeclaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<MethodDeclarationSyntax>().First();
+
+            context.RegisterCodeFix(CodeAction.Create("Add suffix", x => AddSuffixAsync(context.Document, methodDeclaration, context.CancellationToken)), diagnostic);
+        }
+
+        private async Task<Solution> AddSuffixAsync(Document document, MethodDeclarationSyntax methodDeclaration, CancellationToken cancellationToken)
+        {
+            var methodSymbol = (await document.GetSemanticModelAsync(cancellationToken)).GetDeclaredSymbol(methodDeclaration);
+            return await Renamer.RenameSymbolAsync(
+                document.Project.Solution,
+                methodSymbol,
+                methodDeclaration.Identifier.Text + "Async",
+                document.Project.Solution.Workspace.Options,
+                cancellationToken);
+        }
+    }
+}
